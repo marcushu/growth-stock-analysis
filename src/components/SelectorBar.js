51095e17b6ed48c3003ba9a7f2f8be535c5830fa
marcus <marcushurn@hotmail.com>
2020-08-21 20:35:43 -0700

    now with hooks

--- src/components/SelectorBar.js
+++ src/components/SelectorBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import BigButton from './BigButton';
@@ -7,40 +7,11 @@ import thumbsUp from '../assets/icons8-thumbs-up-64.png';
 import checklist from '../assets/icons8-pass-fail-64.png';
 import calculator from '../assets/icons8-calculator-64.png';
 
-class SelectorBar extends React.Component {
-  constructor() {
-    super();
+export default function SelectorBar() {
+  let [disqualifySelected, setDisqualifySelected] = useState(true);
+  let [questionaireSelected, setQuestionaireSelected] = useState(false);
+  let [FfitnessSelected, setFfitnessSelected] = useState(false);
 
-    this.state = {
-      disqualifySelected: true,
-      questionaireSelected: false,
-      FfitnessSelected: false
-    }
-  }
-
-  updateButton = (caption) => {
-    this.setState(
-      {
-        disqualifySelected: false,
-        questionaireSelected: false,
-        FfitnessSelected: false
-      }, () => {
-        switch (caption) {
-          case "Fiscal Fitness":
-            this.setState({ FfitnessSelected: true });
-            break;
-          case "Performance":
-            this.setState({ questionaireSelected: true });
-            break;
-          default:
-            this.setState({ disqualifySelected: true });
-            break;
-        }
-      });
-
-  }
-
-  render() {
     return (
       <Row className="m-1 p-3 shadow" style={{ backgroundColor: "white" }}>
         <Col className="d-flex justify-content-around">
@@ -49,16 +20,25 @@ class SelectorBar extends React.Component {
               caption="Quick Pre-qualify"
               image={thumbsUp}
               altTxt={"Quick Pre-qualify"}
-              selectedState={this.state.disqualifySelected}
-              selectedHandler={this.updateButton} />
+              selectedState={disqualifySelected}
+              //selectedHandler={this.updateButton} />
+              selectedHandler={() => { 
+                setDisqualifySelected(disqualifySelected = true);
+                setQuestionaireSelected(questionaireSelected = false);
+                setFfitnessSelected(FfitnessSelected = false);
+                }} />
           </Link>
           <Link to="/questionaire">
             <BigButton
               caption="Performance"
               image={checklist}
               altTxt={"Performance"}
-              selectedState={this.state.questionaireSelected}
-              selectedHandler={this.updateButton}
+              selectedState={questionaireSelected}
+              selectedHandler={() => {
+                setDisqualifySelected(disqualifySelected = false);
+                setQuestionaireSelected(questionaireSelected = true);
+                setFfitnessSelected(FfitnessSelected = false);
+              }}
             />
           </Link>
           <Link to="/fiscalFitness">
@@ -66,13 +46,14 @@ class SelectorBar extends React.Component {
               caption="Fiscal Fitness"
               image={calculator}
               altTxt={"Fiscal Fitness"}
-              selectedState={this.state.FfitnessSelected}
-              selectedHandler={this.updateButton} />
+              selectedState={FfitnessSelected}
+              selectedHandler={() => {
+                setDisqualifySelected(disqualifySelected = false);
+                setQuestionaireSelected(questionaireSelected = false);
+                setFfitnessSelected(FfitnessSelected = true);
+              }} />
           </Link>
         </Col>
       </Row>
     )
-  }
 }
-
-export default SelectorBar
